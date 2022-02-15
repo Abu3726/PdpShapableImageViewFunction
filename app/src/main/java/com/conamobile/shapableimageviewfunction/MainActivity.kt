d8e043e29fb78dc1dfc6ce621f681f155252e700
ConaMobile <conamobiledev@gmail.com>
2022-02-15 14:46:10 +0500

    PdpShapableImageViewFunction

--- app/src/main/java/com/conamobile/shapableimageviewfunction/MainActivity.kt
+++ app/src/main/java/com/conamobile/shapableimageviewfunction/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.conamobile.shapableimageviewfunction
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView:TextView = findViewById(R.id.textView)
+
+        val text = "Obidjonov #Nurmuhammad"
+        val spannable: Spannable = SpannableString(text)
+        spannable.setSpan(
+            ForegroundColorSpan(Color.BLUE),
+            10,
+            text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textView.text = spannable
     }
 }
