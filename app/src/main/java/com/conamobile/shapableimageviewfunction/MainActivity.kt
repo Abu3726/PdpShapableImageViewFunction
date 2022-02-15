5044c713f414599b977b481b5032fdeb161afc60
ConaMobile <conamobiledev@gmail.com>
2022-02-15 14:51:53 +0500

    PdpShapableImageViewFunction

--- app/src/main/java/com/conamobile/shapableimageviewfunction/MainActivity.kt
+++ app/src/main/java/com/conamobile/shapableimageviewfunction/MainActivity.kt
@@ -14,12 +14,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val textView:TextView = findViewById(R.id.textView)
 
-        val text = "Obidjonov #Nurmuhammad"
+        val text = "I would #like to do #something similar to the #Twitter app"
         val spannable: Spannable = SpannableString(text)
         spannable.setSpan(
-            ForegroundColorSpan(Color.BLUE),
-            10,
-            text.length,
+            ForegroundColorSpan(Color.GREEN),
+            8,
+            13,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            ForegroundColorSpan(Color.GREEN),
+            20,
+            30,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            ForegroundColorSpan(Color.GREEN),
+            46,
+            54,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
         textView.text = spannable
